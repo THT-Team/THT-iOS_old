472a81fa24c3f4d5a569357f8fa65bd76a9cc269
kangho lee <ibcylon@naver.com>
2023-07-10 21:45:53 +0900

    [Chore] remove main storyboard

---
+++ Falling/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  Falling
+//
+//  Created by Kanghos on 2023/07/10.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+}
