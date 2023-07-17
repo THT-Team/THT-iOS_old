97866cdb149d601d51921324a30913258e4d0c72
hoo <hoo@burritowallet.com>
2023-07-17 23:43:56 +0900

    feat: DesignSystem - add font, temporary splashView and signinRootView

---
+++ Falling/Feature/SignUp/SignUpRootViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpRootViewController.swift
+//  Falling
+//
+//  Created by Hoo's MacBookPro on 2023/07/16.
+//
+
+import UIKit
+
+import RxSwift
+import RxKeyboard
+
+
+class SignUpRootViewController: UIViewController {
+	
+	override func loadView() {
+		super.loadView()
+		self.view.backgroundColor = .white
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+    
+
+}
